20ceb9251fc9d40232a8310689b6a34144b18c9c
NicholasZolton <78943323+NicholasZolton@users.noreply.github.com>
2024-09-07 23:09:59 -0500

    Add mappings/config for chat, neorg latex support

---
+++ lua/configs/neorg.lua
@@ -0,0 +1,37 @@
+local options = {
+  load = {
+    ["core.defaults"] = {}, -- Loads default behaviour
+    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+    ["core.dirman"] = { -- Manages Neorg workspaces
+      config = {
+        workspaces = {
+          notes = "~/orgfiles",
+        },
+        default_workspace = "notes",
+        index = "dashboard.norg",
+      },
+    },
+    ["core.export"] = {}, -- Allows for the exporting of documents using the ":Neorg export file_path" command
+    ["core.export.markdown"] = {}, -- Allows for the exporting of documents using the ":Neorg export file_path" command
+    ["core.completion"] = {
+      config = {
+        engine = "nvim-cmp",
+      },
+    }, -- Allows for the exporting of documents using the ":Neorg export file_path" command
+    ["core.esupports.metagen"] = {
+      config = {
+        author = "NicholasZolton",
+        type = "auto",
+      },
+    },
+    ["core.integrations.nvim-cmp"] = {},
+  },
+}
+if vim.fn.has "win32" == 1 then
+else
+  -- add the latex renderer
+  options.load["core.latex.renderer"] = {
+    render_on_enter = true,
+  }
+end
+return options
