a1168c8f19be382b7043e315cb70a41375021d0e
Nicholas Zolton <nicholaszolton@gmail.com>
2024-12-07 02:49:16 -0600

    Put oil opts in a separate file

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -15,33 +15,8 @@ local plugins = {
         end
       end
 
-      require("oil").setup {
-        delete_to_trash = true,
-        --watch_for_changes = true,
-        view_options = {
-          show_hidden = true,
-        },
-        keymaps = {
-          ["g?"] = { "actions.show_help", mode = "n" },
-          ["<CR>"] = { "actions.select", mode = "n" },
-          ["ov"] = { "actions.select", opts = { vertical = true }, mode = "n" },
-          ["oh"] = { "actions.select", opts = { horizontal = true }, mode = "n" },
-          ["ot"] = { "actions.select", opts = { tab = true }, mode = "n" },
-          ["p"] = { "actions.preview", mode = "n" },
-          ["q"] = { "actions.close", mode = "n" },
-          ["r"] = { "actions.refresh", mode = "n" },
-          ["-"] = { "actions.parent", mode = "n" },
-          ["_"] = { "actions.open_cwd", mode = "n" },
-          ["`"] = { "actions.cd", mode = "n" },
-          ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-          ["S"] = { "actions.change_sort", mode = "n" },
-          ["s"] = { "actions.open_external", mode = "n" },
-          ["g."] = { "actions.toggle_hidden", mode = "n" },
-          ["g\\"] = { "actions.toggle_trash", mode = "n" },
-        },
-        -- Set to false to disable all of the above keymaps
-        use_default_keymaps = false,
-      }
+      local opts = require "configs.oilopts"
+      require("oil").setup(opts)
     end,
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
