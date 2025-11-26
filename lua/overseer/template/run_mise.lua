f5c4728cb4aceadd4bf77bd88224db542121f65f
Nicholas Zolton <nmisc@zolton.me>
2025-11-26 01:25:19 -0600

    working mise in overseer 2.0.0

--- lua/overseer/template/run_mise.lua
+++ lua/overseer/template/run_mise.lua
@@ -1,30 +1,16 @@
 local log = require "overseer.log"
 local overseer = require "overseer"
 
----@type overseer.TemplateFileDefinition
-local tmpl = {
-  priority = 60,
-  params = {
-    args = { type = "list", delimiter = " " },
-    cwd = { optional = true },
-  },
-  builder = function(params)
-    local cmd = { "mise", "run" }
-    return {
-      args = params.args,
-      cmd = cmd,
-      cwd = params.cwd,
-    }
-  end,
-}
+---@param name string
+---@return boolean
+local function is_misefile(name)
+  name = name:lower()
+  return name == "mise.toml" or name == ".mise.toml"
+end
 
 ---@param opts overseer.SearchParams
 ---@return nil|string
 local function get_mise_file(opts)
-  local is_misefile = function(name)
-    name = name:lower()
-    return name == "mise.toml" or name == ".mise.toml"
-  end
   return vim.fs.find(is_misefile, { upward = true, path = opts.dir })[1]
 end
 
@@ -33,51 +19,46 @@ local provider = {
   cache_key = function(opts)
     return get_mise_file(opts)
   end,
-  condition = {
-    callback = function(opts)
-      if vim.fn.executable "mise" == 0 then
-        return false, 'Command "mise" not found'
-      end
-      if not get_mise_file(opts) then
-        return false, "No mise.toml found"
-      end
-      return true
-    end,
-  },
   generator = function(opts, cb)
+    if vim.fn.executable "mise" == 0 then
+      return 'Command "mise" not found'
+    end
+    local misefile = get_mise_file(opts)
+    if not misefile then
+      return "No mise.toml found"
+    end
+    local cwd = vim.fs.dirname(misefile)
     local ret = {}
-    local jid = vim.fn.jobstart({ "mise", "tasks", "--json" }, {
-      stdout_buffered = true,
-      on_stdout = vim.schedule_wrap(function(_, output)
-        local ok, data = pcall(vim.json.decode, table.concat(output, ""), { luanil = { object = true } })
+    overseer.builtin.system(
+      { "mise", "tasks", "--json" },
+      { cwd = cwd, text = true },
+      vim.schedule_wrap(function(out)
+        if out.code ~= 0 then
+          cb(out.stderr or out.stdout or "Error running 'mise tasks'")
+          return
+        end
+        local ok, data = pcall(vim.json.decode, out.stdout, { luanil = { object = true } })
         if not ok then
-          log:error("mise produced invalid json: %s\n%s", data, output)
-          cb(ret)
+          log.error("mise produced invalid json: %s", out.stdout)
+          cb(string.format("mise produced invalid json: %s", data))
           return
         end
         assert(data)
-        for _, value in pairs(data) do
-          table.insert(
-            ret,
-            overseer.wrap_template(tmpl, {
-              name = string.format("mise %s", value.name),
-              desc = value.description ~= "" and value.description or nil,
-            }, {
-              args = { value.name },
-              cwd = opts.dir,
-            })
-          )
+        for _, task in pairs(data) do
+          table.insert(ret, {
+            name = string.format("mise %s", task.name),
+            desc = task.description ~= "" and task.description or nil,
+            builder = function()
+              return {
+                cmd = { "mise", "run", task.name },
+                cwd = cwd,
+              }
+            end,
+          })
         end
         cb(ret)
-      end),
-    })
-    if jid == 0 then
-      log:error 'Passed invalid arguments to "mise tasks"'
-      cb(ret)
-    elseif jid == -1 then
-      log:error '"mise" is not executable'
-      cb(ret)
-    end
+      end)
+    )
   end,
 }
 
